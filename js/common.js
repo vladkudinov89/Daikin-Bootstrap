89e4d78f89055f6f8b893bc8396b4d0b432d9b22
Vlad Kudinov <vlad.kudinov.89@gmail.com>
2017-06-18 19:52:48 +0300

    version 18-06

--- js/common.js
+++ js/common.js
@@ -1,12 +1,25 @@
-
 $(document).ready(function () {
+
+    $('.owl-carousel').owlCarousel({
+
+        items: 1,
+        nav: true,
+        loop: true,
+
+        navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"]
+
+    });
+
+   /* $( ".owl-prev").html('<i class="fa fa-chevron-left"></i>');
+    $( ".owl-next").html('<i class="fa fa-chevron-right"></i>');*/
+
 
     // Add scrollspy to <body>
     $('body').scrollspy({target: ".navbar", offset: 70});
 
 
     // Add smooth scrolling on all links inside the navbar
-    $("#myNavbar a").on('click', function(event) {
+    $("#myNavbar a").on('click', function (event) {
         // Make sure this.hash has a value before overriding default behavior
         if (this.hash !== "") {
             // Prevent default anchor click behavior
@@ -19,7 +32,7 @@ $(document).ready(function () {
             // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
-            }, 500, function(){
+            }, 500, function () {
 
                 // Add hash (#) to URL when done scrolling (default click behavior)
                 window.location.hash = hash;
