d5799b185076e3d361221424ad5c0b415613a62e
vladkudinov89 <vlad.kudinov.89@gmail.com>
2017-06-25 16:46:52 +0300

    version 24-06 finall Version(with shiv)

--- js/common.js
+++ js/common.js
@@ -66,7 +66,7 @@
                 }, 500, function () {
 
                     // Add hash (#) to URL when done scrolling (default click behavior)
-                    window.location.hash = '';
+                   /* window.location.hash = '';*/
 
                 });
                 $(".navbar-collapse").collapse('hide');
